4a4c3fea90088ee59384e87c1d6aff973812bd2c
Matt McCormick <matt@mmmccormick.com>
2025-03-30 20:05:08 -0400

    More logos

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,13 @@
 import "./style.css";
-import typescriptLogo from "./typescript.svg";
-import viteLogo from "/vite.svg";
 
-// document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-//   <div>
-//     <a href="https://vite.dev" target="_blank">
-//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
-//     </a>
-//     <a href="https://www.typescriptlang.org/" target="_blank">
-//       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-//     </a>
-//     <h1>Vite + TypeScript</h1>
-//   </div>
-// `;
+import { Niivue } from 'npm:@niivue/niivue';
+
+const niiVueDefaults = {
+  backColor: [0.2, 0.2, 0.2, 1],
+  show3Dcrosshair: false,
+  loglevel: 'info',
+  isRuler: false,
+  gradientOpacity: 0.7,
+}
+const nv1 = new Niivue(niiVueDefaults);
+const gl1 = document.getElementById("gl1")!;
